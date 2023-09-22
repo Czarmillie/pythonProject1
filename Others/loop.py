768f1e341ec97fab434637063b46e40d77b0df42
Olanrewaju Daniel <134516585+Czarmillie@users.noreply.github.com>
2023-09-22 06:23:49 +0100

    Delete Others directory

--- Others/loop.py
+++
@@ -1,9 +0,0 @@
-power = 0
-num = 2
-
-while num <= 2000:
-    power += 1
-    num = 2 ** power
-
-print("Result:", power)
-print("Result:", num)
--- Others/password.py
+++
@@ -1,5 +0,0 @@
-password = input('Enter your Password: ')
-while len(password) < 8:
-    password = input('Enter your Password: ')
-if len(password) >= 8:
-    print(f'Your password is secure, The length is: {len(password)} ')
