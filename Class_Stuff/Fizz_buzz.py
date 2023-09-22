322d45b1c6de2bafdfdbbf931cd0210abcf91a0d
Olanrewaju Daniel <134516585+Czarmillie@users.noreply.github.com>
2023-09-22 06:23:06 +0100

    Delete Class_Stuff directory

--- Class_Stuff/Fizz_buzz.py
+++
@@ -1,9 +0,0 @@
-for num in range(1, 50):
-    if num % 3 == 0 and num % 5 == 0:
-        print("Fizz Buzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
